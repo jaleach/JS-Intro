aa533a5af42432a0bd92fb428a136bbb6804fa26
jim leach <yogajim@gmail.com>
2015-05-11 14:14:59 -0400

    Initial commit JS-Intro

---
+++ main.js
@@ -0,0 +1,52 @@
+var givenName;
+// Q: What does `givenName` equal right now?
+// its just a variable and undefined
+
+givenName = "Tim";
+// Q: What is `givenName` set to right now?
+// it is set to the string "Tim"
+
+givenName = "JD";
+// Q: What is `givenName` set to right now?
+// it is set to the string "JD"
+
+
+var greeting = "Hello, how are you?";
+// Q: What is `greeting` set to?
+// greeting is set to the string "Hello, how are you?"
+
+var high = 50;
+var low  = 10;
+
+
+var math = high - low;
+// Q: What is `math` set to?
+// it is set to the number of 40
+
+
+math = high - "5";
+// Q: What is `math` set to?
+// math is set to the number 45
+
+
+// Create a variable to calculate Tim's age
+// The answer should read "Tim is 31 years old"
+var born = 1983;
+var today = 2015;
+var age = "Tim is " + (today - born) + " years old"
+
+
+
+// Store some information following in variables.
+var yourName = "Jim";
+var instructorName = "Tim";
+
+var statement = yourName + " is taking a class at The Iron Yard, my instructor's name is " + instructorName;
+
+// Jim is taking a class at The Iron Yard, my instructor's name is Tim
+
+
+
+
+
+
